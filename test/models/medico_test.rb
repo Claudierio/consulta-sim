c0986ea9b2dd1681fffd9670742f05ef974b44e8
Claudierio Baltazar <claudierio@gmail.com>
2023-06-30 11:17:37 -0300

    implementação dos testes do model medico

--- test/models/medico_test.rb
+++ test/models/medico_test.rb
@@ -1,7 +1,23 @@
-require "test_helper"
+require 'test_helper'
 
 class MedicoTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  def setup
+    @medico = Medico.new(nome: "Dr. Adolfo",
+                         dataNascimento: Date.new(1980, 1, 1),
+                         cpf: "12345678901",
+                         email: "adolfo@example.com",
+                         especialidade: "Cardiologia",
+                         crm: "12345")
+  end
+
+  test 'criando medico corretamente' do
+    assert @medico.save
+  end
+
+  test "nome deve ser obrigatório" do
+    @medico.nome = ""
+    assert_not @medico.valid?
+  end
+
+
 end
