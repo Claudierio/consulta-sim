3a1bd2715d8bd71f822c147604a5c25f2b63a410
Claudierio Baltazar <claudierio@gmail.com>
2023-06-30 16:14:22 -0300

    implementação dos testes do model paciente

--- test/models/paciente_test.rb
+++ test/models/paciente_test.rb
@@ -1,7 +1,29 @@
-require "test_helper"
+require 'test_helper'
 
 class PacienteTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  def setup
+    @paciente = Paciente.new(
+      nome: "João Silva",
+      dataNascimento: Date.today,
+      cpf: "123456789",
+      email: "joao@example.com",
+      endereco_attributes: {
+        logradouro: "Rua A",
+        complemento: "Apartamento 123",
+        cep: "12345-678",
+        bairro: "Centro",
+        cidade: "São Paulo"
+      }
+    )
+  end
+
+  test "deve ser válido" do
+    assert @paciente.valid?
+  end
+
+  test "dataNascimento deve estar no passado" do
+    @paciente.dataNascimento = Date.tomorrow
+    assert_not @paciente.valid?
+    assert_includes @paciente.errors[:dataNascimento], "não pode ser no futuro"
+  end
 end
